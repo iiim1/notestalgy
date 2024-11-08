b7b66f1a1fadc03279a02a7cc3963587d1d23224
iiim1 <162287433+iiim1@users.noreply.github.com>
2024-11-09 00:42:08 +0800

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -14,11 +14,11 @@
     $judul = [
         'index.php' => 'Notestalgy | Temukan Aroma Khas yang Anda Cintai.',
         'tampilNotes.php' => 'Notes | Notestalgy',
-        'tampilnotes.php' => 'Notes | Notestalgy',  // Lowercase version
+        'tampilnotes.php' => 'Notes | Notestalgy', 
         'tampilMerk.php' => 'Merk | Notestalgy',
-        'tampilmerk.php' => 'Merk | Notestalgy',  // Lowercase version
+        'tampilmerk.php' => 'Merk | Notestalgy',  
         'tampilParfum.php' => 'Parfum | Notestalgy',
-        'tampilparfum.php' => 'Parfum | Notestalgy',  // Lowercase version
+        'tampilparfum.php' => 'Parfum | Notestalgy',  
         'registrasi.php' => 'Daftarkan Akun Anda | Notestalgy',
         'login.php' => 'Masuk ke Akun Anda | Notestalgy',
         'dashboard.php' => 'Dashboard Admin | Notestalgy',
@@ -28,29 +28,29 @@
         'ulasan.php' => 'Ulasan | Notestalgy',
         'pengguna.php' => 'Pengguna | Notestalgy',
         'tambahNotes.php' => 'Tambah Notes | Notestalgy',
-        'tambahnotes.php' => 'Tambah Notes | Notestalgy',  // Lowercase version
+        'tambahnotes.php' => 'Tambah Notes | Notestalgy', 
         'tambahMerk.php' => 'Tambah Merk | Notestalgy',
-        'tambahmerk.php' => 'Tambah Merk | Notestalgy',  // Lowercase version
+        'tambahmerk.php' => 'Tambah Merk | Notestalgy', 
         'tambahParfum.php' => 'Tambah Parfum | Notestalgy',
-        'tambahparfum.php' => 'Tambah Parfum | Notestalgy',  // Lowercase version
+        'tambahparfum.php' => 'Tambah Parfum | Notestalgy',  
         'tambahUlasan.php' => 'Tambah Ulasan | Notestalgy',
-        'tambahulasan.php' => 'Tambah Ulasan | Notestalgy',  // Lowercase version
+        'tambahulasan.php' => 'Tambah Ulasan | Notestalgy',  
         'tambahUser.php' => 'Tambah Pengguna | Notestalgy',
-        'tambahuser.php' => 'Tambah Pengguna | Notestalgy',  // Lowercase version
+        'tambahuser.php' => 'Tambah Pengguna | Notestalgy',  
         'editNotes.php' => 'Perbarui Notes | Notestalgy',
-        'editnotes.php' => 'Perbarui Notes | Notestalgy',  // Lowercase version
+        'editnotes.php' => 'Perbarui Notes | Notestalgy',  
         'editMerk.php' => 'Perbarui Merk | Notestalgy',
-        'editmerk.php' => 'Perbarui Merk | Notestalgy',  // Lowercase version
+        'editmerk.php' => 'Perbarui Merk | Notestalgy',  
         'editParfum.php' => 'Perbarui Parfum | Notestalgy',
-        'editparfum.php' => 'Perbarui Parfum | Notestalgy',  // Lowercase version
+        'editparfum.php' => 'Perbarui Parfum | Notestalgy',  
         'editUlasan.php' => 'Perbarui Ulasan | Notestalgy',
-        'editulasan.php' => 'Perbarui Ulasan | Notestalgy',  // Lowercase version
+        'editulasan.php' => 'Perbarui Ulasan | Notestalgy',  
         'editUser.php' => 'Perbarui Pengguna | Notestalgy',
-        'edituser.php' => 'Perbarui Pengguna | Notestalgy',  // Lowercase version
+        'edituser.php' => 'Perbarui Pengguna | Notestalgy', 
         'detailParfum.php' => 'Detail Parfum | Notestalgy',
-        'detailparfum.php' => 'Detail Parfum | Notestalgy',  // Lowercase version
+        'detailparfum.php' => 'Detail Parfum | Notestalgy', 
         'tambahUlasan.php' => 'Tambah Ulasan | Notestalgy',
-        'tambahulasan.php' => 'Tambah Ulasan | Notestalgy',  // Lowercase version
+        'tambahulasan.php' => 'Tambah Ulasan | Notestalgy',  
     ];
 
     if (array_key_exists($halamanSekarang, $judul)) {
